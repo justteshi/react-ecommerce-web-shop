aecfc8f96de60f06ac96f61415d39fb62a1228e6
thristov <thristov@abrites.com>
2021-01-14 18:07:15 +0200

    Checkout and AdressForm, CheckoutToken is null ???

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,23 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core'
 
+import { commerce } from '../../../lib/commerce'
 import useStyles from './styles'
 import AddressForm from '../AddressFrom'
 import PaymentForm from '../PaymentForm'
 
 const steps = ['Sipping address', 'Payment details']
 
-function Checkout() {
+function Checkout({ cart }) {
     const [activeStep, setActiveStep] = useState(0)
+    const [checkoutToken, setCheckoutToken] = useState(0)
     const classes = useStyles()
+
+    useEffect(() => {
+        const generateToken = async () => {
+            try {
+                const token = await commerce.checkout.generateToken(cart.id, { type:'cart' })
+                console.log(token)
+                setCheckoutToken(token)
+            } 
+            catch (error) {
+                
+            }
+        }
+        generateToken()
+    }, [])
 
     const Confirmation = () => (
         <div>
             Confirmation
         </div>
     )
+    
 
-    const Form = () => activeStep === 0 ? <AddressForm /> : <PaymentForm />
+    const Form = () => activeStep === 0 ? <AddressForm checkoutToken={checkoutToken} /> : <PaymentForm />
 
     return (
         <>
